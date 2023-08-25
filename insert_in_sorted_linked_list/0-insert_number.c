db2ff287c6efc624b5adbb765e272f404c0fdbec
pipeleon <oflg94@gmail.com>
2023-08-25 17:15:46 -0500

    first version insert number sorted linked list

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/* Task 0 - Insert in sorted linked list  */
+
+/**
+ * insert_node - insert in sorted linked list
+ * @head: Head of the list
+ * @number: Number to add
+ *
+ * Return: pointer to a list_t
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+
+	if ((*head)->n >= number)
+	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = *head;
+		*head = new;
+	}
+	else
+		new = add_nodeint_end(&((*head)->next), n);
+
+	return (new);
+}
